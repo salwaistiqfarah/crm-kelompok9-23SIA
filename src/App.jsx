95d0416eece5ba477dbd63b03f5bd8f9e7b8e7d4
Jelly <jelly23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:24 +0700

    penjualan sudah oke by Jelly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import MainLayout from './components/MainLayout'
+import SalesManagement from './Pages/SalesManagement'
 
 export function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/penjualan" element={<SalesManagement />} />
       </Route>
     </Routes>
   )
