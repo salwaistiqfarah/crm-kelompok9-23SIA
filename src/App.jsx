9efed9852f0c8dd8fe5683a259d8323901741397
Hanifa <hanifa23si@mahasiswa.pcr.ac.id>
2025-05-26 15:05:55 +0700

    Produk sudah ok by hanifa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import MainLayout from './components/MainLayout'
+import SalesManagement from './Pages/SalesManagement'
+import ProductManagement from './Pages/Produk'
 
 export function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+         <Route path="/Penjualan" element={<SalesManagement />} />
+          <Route path="/Produk" element={<ProductManagement />} />
       </Route>
     </Routes>
   )
