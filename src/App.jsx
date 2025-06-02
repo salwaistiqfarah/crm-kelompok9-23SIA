7384e7cf742c325816b8a612e0af60c034de60b4
Hanifa <hanifa23si@mahasiswa.pcr.ac.id>
2025-06-02 15:05:13 +0700

    Auth by hanifa

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import MainLayout from './components/MainLayout'
 import SalesManagement from './Pages/SalesManagement'
 import ProductManagement from './Pages/Produk'
 import CustomerManagement from './Pages/CustomerManagement'
+import Login from './Pages/Auth/Login'
+import Register from './Pages/Auth/Register'
 
 export function App() {
   return (
     <Routes>
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
          <Route path="/Penjualan" element={<SalesManagement />} />
