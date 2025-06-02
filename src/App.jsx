6204f2f179413ff4442df6e5b5e7e4322b091251
jelly <jelly23si@mahasiswa.pcr.ac.id>
2025-06-02 14:53:30 +0700

    history by jelly

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import MainLayout from './components/MainLayout'
 import SalesManagement from './Pages/SalesManagement'
 import ProductManagement from './Pages/Produk'
 import CustomerManagement from './Pages/CustomerManagement'
+import Feedback from './Pages/History/Feedback'
+import VisitHistory from './Pages/History/VisitHistory'
 
 export function App() {
   return (
@@ -13,6 +15,8 @@ export function App() {
          <Route path="/Penjualan" element={<SalesManagement />} />
           <Route path="/Produk" element={<ProductManagement />} />
            <Route path="/Pelanggan" element={<CustomerManagement/>} />
+           <Route path="/Feedback" element={<Feedback/>} />
+           <Route path="/VisitHistory" element={<VisitHistory/>} />
       </Route>
     </Routes>
   )
